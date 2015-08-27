b206f2c0d3a773c9df8c814c4627e79df339e5b9
Francis Chong <francis@ignition.hk>
2015-08-27 15:15:29 +0800

    construct a new token object

--- OAuthSwift/OAuthTwitter.swift
+++ OAuthSwift/OAuthTwitter.swift
@@ -47,7 +47,7 @@ public class OAuthTwitter: OAuth1Swift {
                 }
                 
                 if oauthToken != nil && oauthTokenSecret != nil && screenName != nil && userId != nil {
-                    let credential = self.client.credential as! OAuthTwitterCredential
+                    let credential = OAuthTwitterCredential(consumer_key: self.consumer_key, consumer_secret: self.consumer_secret)
                     credential.oauth_token = oauthToken!
                     credential.oauth_token_secret = oauthTokenSecret!
                     credential.screen_name = screenName!
